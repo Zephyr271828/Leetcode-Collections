ecf15a075f915cd84f95d7d8a5bfea8018841990
Yufeng Xu <109715540+Zephyr271828@users.noreply.github.com>
2023-08-03 13:20:07 +0800

    Add files via upload

---
+++ IsSameTree.java
@@ -0,0 +1,30 @@
+package Leetcode;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class IsSameTree {
+    public static boolean isSameTree(TreeNode p, TreeNode q){
+        if (p == null && q == null) return true;
+        if (p == null && q != null) return false;
+        if (p != null && q == null) return false;
+        if (p.val != q.val) return false;
+        else return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static boolean isSameTree2(TreeNode p, TreeNode q){
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
