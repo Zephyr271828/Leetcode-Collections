22fbcd43dabe4da672a2e60fada2b05b9fa12bfa
Yufeng Xu <109715540+Zephyr271828@users.noreply.github.com>
2023-08-08 11:19:30 +0800

    Add files via upload

---
+++ PreorderTraversal.java
@@ -0,0 +1,27 @@
+package Leetcode;
+
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class PreorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root){
+        if (root == null) return new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        ans.add(root.val);
+        ans.addAll(preorderTraversal(root.left));
+        ans.addAll(preorderTraversal(root.right));
+        return ans;
+    }
+}
